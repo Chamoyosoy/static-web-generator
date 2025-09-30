d511e10802f93c38e79fe349662e439046ffd71e
chamoyosoy <chamoyosoy@hotmail.com>
2025-09-30 03:32:13 -0600

    make it recursive

--- src/copy_static.py
+++ src/copy_static.py
@@ -1,21 +1,18 @@
 import os
 import shutil
 
-def copy_static(target_dir, working_dir):
+def copy_static(working_dir, target_dir):
     
-    try:
-        for filename in os.listdir(working_dir):
-            src_path = os.path.join(working_dir, filename)
-            dst_path = os.path.join(target_dir, filename)
-
+    
+    for filename in os.listdir(working_dir):
+        src_path = os.path.join(working_dir, filename)
+        dst_path = os.path.join(target_dir, filename)
+        try:
             if os.path.isdir(src_path):
                 os.mkdir(dst_path)
-                for obj in os.listdir(src_path):
-                    src_path2 = os.path.join(src_path, obj)
-                    dst_path2 = os.path.join(dst_path, obj)
-                    shutil.copy(src_path2, dst_path2)
+                copy_static(src_path, dst_path)
             else:
                 shutil.copy(src_path, dst_path)
 
-    except Exception as e:
-        return f'Error: {e}'
+        except Exception as e:
+            return f'Error: {e}'
